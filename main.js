142e183894898da6190d318b1f7dec8864719e7d
marcinpm <marcinpm69@gmail.com>
2023-08-25 18:34:06 +0200

    rozpoczecie zadania

---
+++ main.js
@@ -0,0 +1,12 @@
+let number = 12;
+
+if (number === 12) {
+    console.log('Podana liczba jest większa od 10');
+} else if (number === 10) {
+    console.log('Podana liczba nie jest większa od 10');
+} else {
+    console.log('Żaden z powyższych warunków nie został spełniony!')
+}
+
+
+
