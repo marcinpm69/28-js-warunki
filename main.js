932c9a6d4a76035a4fd100160d3fa1dd94a47fa7
marcinpm <marcinpm69@gmail.com>
2023-09-02 16:29:58 +0200

    korekta zadania

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let number = 12;
+let number = 9;
 
-if (number === 12) {
+if (number > 10) {
     console.log('Podana liczba jest większa od 10');
-} else if (number === 10) {
+} else if (number < 10) {
     console.log('Podana liczba nie jest większa od 10');
 } else {
     console.log('Żaden z powyższych warunków nie został spełniony!')
